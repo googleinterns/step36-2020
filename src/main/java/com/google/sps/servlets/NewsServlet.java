d274ae12eaa390d26c64ce6bd2f2dfdb2143c4c1
Nathan Safir <nssafir@gmail.com>
2020-06-29 22:24:39 +0000

    Format article builders correctly.

--- src/main/java/com/google/sps/servlets/NewsServlet.java
+++ src/main/java/com/google/sps/servlets/NewsServlet.java
@@ -22,13 +22,15 @@ public class NewsServlet extends HttpServlet {
   // In future implementation, will be generated based on key words.
   private List<Article> articles = new ArrayList<>(
       List.of(
-        new Article.Builder("Bolton says Trump turned a blind eye to the coronavirus pandemic",
-        "https://www.cnn.com/2020/06/24/politics/john-bolton-interview-cnntv/index.html")
+        new Article.Builder(
+            "Bolton says Trump turned a blind eye to the coronavirus pandemic",
+            "https://www.cnn.com/2020/06/24/politics/john-bolton-interview-cnntv/index.html")
           .withWriter("Zachary Cohen")
           .withDate("06/24/2020")
           .build(),
-        new Article.Builder("Hearing goes off the rails when lawmaker keeps banging table",
-        "https://www.cnn.com/videos/politics/2020/06/24/louie-gohmert-bangs-table-judiciary-hearing-vpx.cnn")
+        new Article.Builder(
+            "Hearing goes off the rails when lawmaker keeps banging table",
+            "https://www.cnn.com/videos/politics/2020/06/24/louie-gohmert-bangs-table-judiciary-hearing-vpx.cnn")
           .withDate("06/24/2020")
           .build()
       )   
