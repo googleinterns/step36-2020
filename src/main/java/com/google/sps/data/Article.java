1e7cb0623cfed95e3536652354026715cf840891
Nathan Safir <nssafir@gmail.com>
2020-06-29 15:28:50 +0000

    Make changes requested in PR -- extended Article class.

--- src/main/java/com/google/sps/data/Article.java
+++ src/main/java/com/google/sps/data/Article.java
@@ -3,8 +3,12 @@ package com.google.sps.data;
 public class Article {
   private String title;
   private String link;
+  private String writer;
+  private String date;
 
   private Article(Builder b) {
+    this.title = b.title;
+    this.link = b.link;
     this.title = b.title;
     this.link = b.link;
   }
@@ -12,13 +16,21 @@ public class Article {
   public static class Builder {
     private String title;
     private String link;
+    private String writer;
+    private String date;
 
-    public Builder(String title) {
+    public Builder(String title, String link) {
       this.title = title;
+      this.link = link;
     }
 
-    public Builder withLink(String link) {
-      this.link = link;
+    public Builder withWriter(String writer) {
+      this.writer = writer;
+      return this;
+    }
+
+    public Builder withDate(String date) {
+      this.date = date;
       return this;
     }
   
--- src/main/java/com/google/sps/servlets/NewsServlet.java
+++ src/main/java/com/google/sps/servlets/NewsServlet.java
@@ -16,17 +16,20 @@ import com.google.sps.data.Article;
  */
 @WebServlet("/news")
 public class NewsServlet extends HttpServlet {
-
   private List<String> keywords = new ArrayList<>();
+  
   // Hardcoded list of articles. 
   // In future implementation, will be generated based on key words.
   private List<Article> articles = new ArrayList<>(
       List.of(
-        new Article.Builder("Bolton says Trump turned a blind eye to the coronavirus pandemic")
-          .withLink("https://www.cnn.com/2020/06/24/politics/john-bolton-interview-cnntv/index.html")
+        new Article.Builder("Bolton says Trump turned a blind eye to the coronavirus pandemic",
+        "https://www.cnn.com/2020/06/24/politics/john-bolton-interview-cnntv/index.html")
+          .withWriter("Zachary Cohen")
+          .withDate("06/24/2020")
           .build(),
-        new Article.Builder("Hearing goes off the rails when lawmaker keeps banging table")
-          .withLink("https://www.cnn.com/videos/politics/2020/06/24/louie-gohmert-bangs-table-judiciary-hearing-vpx.cnn")
+        new Article.Builder("Hearing goes off the rails when lawmaker keeps banging table",
+        "https://www.cnn.com/videos/politics/2020/06/24/louie-gohmert-bangs-table-judiciary-hearing-vpx.cnn")
+          .withDate("06/24/2020")
           .build()
       )   
   );
@@ -40,7 +43,7 @@ public class NewsServlet extends HttpServlet {
   private String makeNewsJson() {
     Gson gson = new Gson();
     String json = gson.toJson(articles);
-    json = "{\"articles\": " + json + "}";  
+    json = String.format("{\"articles\": %s}", json);
     return json;
   }
 }
