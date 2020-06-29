5725587980d1eeb5d7986c96b2e4aeab64f609b2
Juan Uribe <juanjouribev1@gmail.com>
2020-06-29 16:25:34 +0000

    Add servlet to query Keywords

---
+++ src/main/java/com/google/sps/servlets/KeywordServlet.java
@@ -0,0 +1,54 @@
+
+package com.google.sps.servlets;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Given a tweet or sentence, the servlet will extract salient terms from those
+ * and store them in a list.
+ */
+@WebServlet("/keyword")
+public class KeywordServlet extends HttpServlet {
+
+  private static final List<String> tweets = new ArrayList<>();
+  private static final List<String> terms = Arrays.asList("Black Lives Matter", "COVID-19");
+
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    // TODO: Extract saliency
+    String json = mustacheJsonBuilder(terms);
+    response.getWriter().println(json);
+  }
+
+  @Override
+  public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    final String tweet = request.getParameter("keyword-sentence");
+    if (tweet != null && !tweet.equals("")) {
+      tweets.add(request.getParameter("keyword-sentence"));
+    }
+    response.sendRedirect("/main.html");
+  }
+
+  /**
+   * Generates and returns a JSON usable by Mustache.
+   */
+  private String mustacheJsonBuilder(Collection<String> toJson) {
+    String builder = "{\"keywords\": [";
+    for (String term : toJson) {
+      builder += "{\"term\": \"" + term + "\"},";
+    }
+
+    // Remove extra comma at the end.
+    builder = builder.substring(0, builder.length() - 1) + "]}";
+    return builder;
+  }
+
+}
