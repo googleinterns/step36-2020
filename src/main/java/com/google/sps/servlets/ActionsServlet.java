eb22652eb45c0dce96e2f444aad3ade079ab0d03
Nathan Safir <nssafir@gmail.com>
2020-06-30 15:10:08 +0000

    Put actual api key in Action Servlet to test.

--- src/main/java/com/google/sps/servlets/ActionsServlet.java
+++ src/main/java/com/google/sps/servlets/ActionsServlet.java
@@ -23,7 +23,7 @@ import java.util.HashMap;
 public class ActionsServlet extends HttpServlet {
 
   private static final List<String> terms = Arrays.asList("Black Lives Matter", "COVID-19");
-  private static final String API_KEY = "API_KEY";  // Insert the API_KEY here for testing.
+  private static final String API_KEY = "a752ec3e-9fcf-4500-9107-694351adc5ee";  // Insert the API_KEY here for testing.
   private static final String API_PATH = "https://api.globalgiving.org/api/public/services/search/projects/summary";
 
   @Override
