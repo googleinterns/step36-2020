10a71b53350971c0801a84070221344eb19c8231
Juan Uribe <juanjouribev1@gmail.com>
2020-06-30 17:19:32 +0000

    Add loader to js and css

--- src/main/webapp/scripts/main.js
+++ src/main/webapp/scripts/main.js
@@ -1,24 +1,20 @@
-$().ready(()=>{
-  loadContentSection().then(()=>{
+$().ready(() => {
+  loadContentSection().then(() =>{ 
+    $("#loader").addClass("hide");
+    $("#real-body").removeClass("hide");
+    $("#real-body").addClass("body");
+
     $(".keyword").click(function() {
       $(this).addClass("active");
       $(".base").hide(200);
-      $("body").addClass("focus");
-      $(".section").addClass('active-url');
-      $(".active-url").click(function(event) {
-        event.stopPropagation();
-        let url = $(this).attr("data-url");
-        window.open(url);
-      });
+      $("#real-body").addClass("focus");
     });
 
     $(".close").click(function(event) {
       event.stopPropagation();
       $(this).parent().removeClass("active");
       $(".base").show(200);
-      $("body").removeClass("focus");
-      $(".active-url").off("click");
-      $(".section").removeClass('active-url');
+      $("#real-body").removeClass("focus");
     });
   });
 });
