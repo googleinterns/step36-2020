84a59336365c5d74279b63b023dd2c23d33a8e4f
Nathan Safir <nssafir@gmail.com>
2020-07-01 00:08:25 +0000

    Implement doGet for location servlet (not yet tested.

--- src/main/java/com/google/sps/servlets/LocationServlet.java
+++ src/main/java/com/google/sps/servlets/LocationServlet.java
@@ -5,14 +5,35 @@ import javax.servlet.http.HttpServletResponse;
 
 @WebServlet("/location")
 public class LocationServlet extends HttpServlet {
-    private Double latitude = 0.0;
-    private Double longitude = 0.0;
+    private double latitude = 0.0;
+    private double longitude = 0.0;
     private final String BASE_URL = "https://maps.googleapis.com/maps/api/geocode/json?";
     private final String API_KEY = ""; // Insert actual API key to test.
 
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) {
-      
+      String queryParam = String.format("latlng=%1$f,%2$f", latitude, longitude);
+      String apiKeyParam = String.format("&key=%s", API_KEY);
+      String fullPath = BASE_URL + queryParam + apiKeyParam;
+      try {
+        URL url = new URL(queryPath);
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setRequestMethod("GET");
+        connection.connect();
+        int responseCode = connection.getResponseCode();
+        if (responseCode == 200) {
+          String json = getJson(url);
+          System.out.println(json); // test
+        } else {
+          System.out.println("Error: connection response code is: " + responseCode);
+        }    
+      } catch(Exception e) {
+        e.printStackTrace();
+     }
+    }
+
+    public void getJson() {
+      // TODO: implement this method.
     }
 
 }
