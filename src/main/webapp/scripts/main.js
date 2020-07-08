eb42e2849f63b33f216dcc316f62d1078da81406
Juan Uribe <juanjouribev1@gmail.com>
2020-07-08 18:41:47 +0000

    Update on click event to de added dynamically to the DOM

--- src/main/webapp/scripts/main.js
+++ src/main/webapp/scripts/main.js
@@ -3,23 +3,24 @@ $().ready(() => {
     $("#loader").addClass("hide");
     $("#real-body").removeClass("hide");
     $("#real-body").addClass("body");
-
-    $(".keyword").click(function() {
-      $(this).addClass("active");
-      $(".base").hide(200);
-      $("#real-body").addClass("focus");
-      $(".active .item").click(function() {
-        $(".item").off("click");
-        const url = $(this).attr("data-url");
-        window.location.href = url;
-      });
-    });
-
-    $(".close").click(function(event) {
-      event.stopPropagation();
-      $(this).parent().removeClass("active");
-      $(".base").show(200);
-      $("#real-body").removeClass("focus");
-    });
   });
+});
+
+$("body").on('click', '.keyword', function() {
+  $(this).addClass("active");
+  $(".base").hide(200);
+  $("#real-body").addClass("focus");
+});
+
+$("body").on('click', ".close", function(event) {
+  event.stopPropagation();
+  $(this).parent().removeClass("active");
+  $(".base").show(200);
+  $("#real-body").removeClass("focus");
+});
+
+$("body").on('click', ".active .item", function() {
+  $(".item").off("click");
+  const url = $(this).attr("data-url");
+  window.location.href = url;
 });
