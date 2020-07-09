50e2507be5d6bc81b4549a2edb410fa613c68b08
Andrew Li <andrewhanzhuoli@gmail.com>
2020-07-09 04:34:36 +0000

    Add JQuery to set up buttons on main page

---
+++ src/main/webapp/scripts/keywords.js
@@ -0,0 +1,18 @@
+$().ready(function() {
+  $("#select-blob").on("click", function() {
+    fetch('/user-image').then(response => response.text()).then((blobURL) => {
+      $("#keyword-form").removeClass("hide").attr("action", blobURL);
+      $("#blob-input").removeClass("hide");
+      $("#text-input").addClass("hide");
+      $("#form-title *").text("Upload an Image Here");
+    });
+  });
+
+  $("#select-text").on("click", function() {
+    $("#keyword-form, #text-input").removeClass("hide");
+    $("#keyword-form").attr("action", "/keyword");
+    $("#blob-input").addClass("hide");
+    $("#form-title *").text("Copy Your Tweet Tere");
+  });
+});
+
