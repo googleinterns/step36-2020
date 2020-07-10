8b5dffe56e61951ded08023bc31e51afb01baa85
Andrew Li <andrewhanzhuoli@gmail.com>
2020-07-10 21:04:25 +0000

    Disable blobstore buttons due to errors

--- src/main/webapp/scripts/keywords.js
+++ src/main/webapp/scripts/keywords.js
@@ -1,4 +1,6 @@
 $().ready(function() {
+  /**
+  // TODO: Fix blobstore integration
   $('#select-blob').on('click', function() {
     fetch('/user-image').then(response => response.text()).then((blobURL) => {
       $('#keyword-form').removeClass('hide').attr('action', blobURL).attr('enctype', 'multipart/form-data');
@@ -6,7 +8,7 @@ $().ready(function() {
       $('#text-input').addClass('hide').val(null);
       $('#form-title *').text('Upload an Image Here');
     });
-  });
+  });*/
 
   $('#select-text').on('click', function() {
     $('#keyword-form, #text-input').removeClass('hide');
