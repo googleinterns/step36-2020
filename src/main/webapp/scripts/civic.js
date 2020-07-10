65bb52ce31ba8031589ea2dc89dae39c54e1410e
Juan Uribe <45736450+juanuribe28@users.noreply.github.com>
2020-07-09 17:45:28 -0700

    Update civic.js

--- src/main/webapp/scripts/civic.js
+++ src/main/webapp/scripts/civic.js
@@ -3,12 +3,13 @@
  */
 function extractOfficials(civicObj) {
   let officials = new Object();
-  let levels = ["administrativeArea1", "administrativeArea2", "country", "international", "locality", "regional", "special", "subLocality1", "subLocality2"];
+  const levels = ["administrativeArea1", "administrativeArea2", "country", "international", "locality", "regional", "special", "subLocality1", "subLocality2"];
   levels.forEach((level) => {
     officials[level] = new Array();
   });
   civicObj.offices.forEach((officeObj) => {
-    let level = officeObj.levels[0];
+    const level = officeObj.levels[0];
+    // Loop through the officials Indices that work at the current office, and add them to their coresponding level.
     officeObj.officialIndices.forEach((officialIndex) => {
       let official = civicObj.officials[officialIndex];
       official.title = officeObj.name;
