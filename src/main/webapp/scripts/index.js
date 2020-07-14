cb88dcbaaa5adbba52b6db13c7bed6a42cf30ca3
Nathan Safir <nssafir@gmail.com>
2020-07-14 15:46:31 +0000

    Fix jQuery for location checkbox.

--- src/main/webapp/scripts/index.js
+++ src/main/webapp/scripts/index.js
@@ -1,1 +1,8 @@
-$("checkbox").click(setLocationCookie);
+//$("checkbox").click(setLocationCookie);
+$(document).ready(function() {
+  $('#location-checkbox').change(function() {
+    if(this.checked) {
+      setLocationCookie();
+    }
+  });
+});
