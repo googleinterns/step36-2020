5b2ff8a6c706db4a3a26cb5aef7c7c783ad55109
Nathan Safir <nssafir@gmail.com>
2020-07-15 17:26:39 +0000

    Remove wildcard import statements.

--- src/test/java/com/google/sps/TestingUtil.java
+++ src/test/java/com/google/sps/TestingUtil.java
@@ -3,10 +3,10 @@ package com.google.sps.servlets;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.StringWriter;
-import java.util.*;
 import org.mockito.Mockito;
 import java.io.PrintWriter;
 import java.io.IOException;
+import java.util.Map;
 
 public class TestingUtil { 
 
