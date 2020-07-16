7b4879dcb86b3e60c1c27c2c2391216814ea2d2b
Andrew Li <andrewhanzhuoli@gmail.com>
2020-07-16 22:03:24 +0000

    Change vanilla to JQuery, remove onsubmit attribute

--- src/main/webapp/scripts/keywords.js
+++ src/main/webapp/scripts/keywords.js
@@ -14,35 +14,33 @@ $().ready(function() {
     $('#blob-input').addClass('hide').val(null);
     $('#form-title *').text('Copy Your Tweet Here');
   });
-});
 
-/** 
- * Checks whether the form inputs are valid before submitting said form.
- */
-function validateForm(event) {
-  if ($('#blob-input').hasClass('hide')) {
-    // Check the text input
-    if ($('#text-input').val() == null || $('#text-input').val() === "") {
-      alert('Your input cannot be empty!'); 
-      event.preventDefault();
-      return false;
-    } else {}
-  } else {  // Check blob input
-    const blobInput = document.getElementById('blob-input');
-    if (blobInput.files.length == 0) {
-      alert("You must upload an image file!");
-      event.preventDefault();
-      return false;
+  $('#keyword-form').on('submit', function(event) {
+    // Check whether the inputs are valid before submitting.
+    if ($('#blob-input').hasClass('hide')) {
+      // Check the text input
+      if ($('#text-input').val() == null || $('#text-input').val() === "") {
+        alert('Your input cannot be empty!'); 
+        event.preventDefault();
+        return false;
+      }
+    } else {  // Check blob input
+      const blobInput = $('#blob-input').prop('files');
+      if (blobInput.length == 0) {
+        alert("You must upload an image file!");
+        event.preventDefault();
+        return false;
+      }
+      const blobFile = blobInput[0];
+      const fileType = blobFile['type'];
+      const validImageTypes = ['image/jpeg', 'image/png', 'image/jpg'];
+      if (blobFile == null || !validImageTypes.includes(fileType)) {
+        alert('You must upload an image file!');
+        event.preventDefault();
+        return false;
+      }    
     }
-    const blobFile = blobInput.files[0];
-    const fileType = blobFile['type'];
-    const validImageTypes = ['image/jpeg', 'image/png', 'image/jpg'];
-    if (blobFile == null || !validImageTypes.includes(fileType)) {
-      alert('You must upload an image file!');
-      event.preventDefault();
-      return false;
-    }    
-  }
-  return true;
-}
+    return true;
+  });
+});
 
