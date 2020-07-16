5ab37683dab42d0b9490d42f26e50cf078ca9b7e
Andrew Li <andrewhanzhuoli@gmail.com>
2020-07-16 19:13:21 +0000

    Add alert for when nothing is inputted

--- src/main/webapp/scripts/keywords.js
+++ src/main/webapp/scripts/keywords.js
@@ -15,3 +15,34 @@ $().ready(function() {
     $('#form-title *').text('Copy Your Tweet Here');
   });
 });
+
+/** 
+ * Checks whether the form inputs are valid before submitting said form.
+ */
+function validateForm(event) {
+  if ($('#blob-input').hasClass('hide')) {
+    // Check the text input
+    if ($('#text-input').val() == null || $('#text-input').val() === "") {
+      alert('Your input cannot be empty!'); 
+      event.preventDefault();
+      return false;
+    } else {}
+  } else {  // Check blob input
+    const blobInput = document.getElementById('blob-input');
+    if (blobInput.files.length == 0) {
+      alert("You must upload an image file!");
+      event.preventDefault();
+      return false;
+    }
+    const blobFile = blobInput.files[0];
+    const fileType = blobFile['type'];
+    const validImageTypes = ['image/jpeg', 'image/png', 'image/jpg'];
+    if (blobFile == null || !validImageTypes.includes(fileType)) {
+      alert('You must upload an image file!');
+      event.preventDefault();
+      return false;
+    }    
+  }
+  return true;
+}
+
