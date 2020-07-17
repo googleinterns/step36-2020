974d851f83ffadba56e30f1c8559dd01de335773
Andrew Li <andrewhanzhuoli@gmail.com>
2020-07-17 16:16:15 +0000

    Adjust thresholds, fix nits, put thresholds in static constant variables

--- src/main/java/com/google/sps/data/Keywords.java
+++ src/main/java/com/google/sps/data/Keywords.java
@@ -34,6 +34,15 @@ public final class Keywords {
 
   private static final int MAX_NUM_KEYWORDS = 10;
 
+  // We don't want any terms that are far too un-salient to include in the keywords list.
+  // 0.2 is a good minimum threshold as, especially when extracting saliency from a list of
+  // label, many of the high-relevancy terms end up only getting assigned a salience of 0.2.
+  private static final int MIN_SALIENCE_THRESHOLD = 0.2;
+
+  // For labels, we want high-confidence labels. Many of the labels below 0.70 seem to end
+  // up being only tangetially related to the contents of the picture.
+  private static final int MIN_SCORE_THRESHOLD = 0.70;
+
   /**
    * @return a list of the 5 most salient keywords
    */
@@ -57,7 +66,7 @@ public final class Keywords {
   public static String addKeywords(List<EntityAnnotation> blobAnalysis) throws IOException {
     List<String> labelList = new ArrayList<>();
     for (EntityAnnotation label : blobAnalysis) {
-      if (label.getScore() >= 0.75) {
+      if (label.getScore() >= MIN_SCORE_THRESHOLD) {
         labelList.add(label.getDescription());
       }
     }
@@ -95,7 +104,7 @@ public final class Keywords {
       }
     });
     for (com.google.cloud.language.v1.Entity entity : entities) {
-      if (entity.getSalience() < 0.3) {
+      if (entity.getSalience() < MIN_SALIENCE_THRESHOLD) {
         continue;
       }
       orderSet.add(entity);
--- src/main/webapp/scripts/keywords.js
+++ src/main/webapp/scripts/keywords.js
@@ -1,8 +1,8 @@
 $().ready(function() {
   $('#select-blob').on('click', function() {
-    fetch('/user-image').then(response => response.text()).then((blobURL) => {
+    fetch('/user-image').then(response => response.json()).then((blobURL) => {
       $('#keyword-form, #blob-input').removeClass('hide');
-      $('#keyword-form').attr('action', blobURL).attr('enctype', 'multipart/form-data');
+      $('#keyword-form').attr('action', blobURL[0]).attr('enctype', 'multipart/form-data');
       $('#text-input').addClass('hide').val(null);
       $('#form-title *').text('Upload an Image Here');
     });
@@ -18,19 +18,21 @@ $().ready(function() {
   $('#keyword-form').on('submit', function(event) {
     // Check whether the inputs are valid before submitting.
     if ($('#blob-input').hasClass('hide')) {
-      // Check the text input
+      // Check whether the text input is null, or has an empty string as a value.
       if ($('#text-input').val() == null || $('#text-input').val() === "") {
         alert('Your input cannot be empty!'); 
         event.preventDefault();
         return false;
       }
-    } else {  // Check blob input
+    } else {
+      // Check whether a file got uploaded to the input tag.
       const blobInput = $('#blob-input').prop('files');
       if (blobInput.length == 0) {
         alert("You must upload an image file!");
         event.preventDefault();
         return false;
       }
+      // Check whether the file is of an image type.
       const blobFile = blobInput[0];
       const fileType = blobFile['type'];
       if (blobFile == null || !fileType.includes('image')) {
