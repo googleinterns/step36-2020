75a66a9bad87cf4c9b9dfea6be5ffbf1559a791d
Juan Uribe <juanjouribev1@gmail.com>
2020-07-21 17:38:58 +0000

    Add js for fake upload button

--- src/main/webapp/scripts/index.js
+++ src/main/webapp/scripts/index.js
@@ -5,4 +5,9 @@ $(document).ready(function() {
       setLocationCookie();
     }
   });
+
+  $('#blob-input').change(function() {
+    const filePath = $(this).val();
+    $('#file-path').text(filePath.split('\\').pop());
+  });
 });
