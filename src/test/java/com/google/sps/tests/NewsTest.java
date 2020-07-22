1e9d621ddb952eb4b469fef3f230503e8b010837
Nathan Safir <nssafir@gmail.com>
2020-07-22 18:12:26 +0000

    Tests for makeArticleList and getHTML functions.

--- src/test/java/com/google/sps/tests/NewsTest.java
+++ src/test/java/com/google/sps/tests/NewsTest.java
@@ -13,6 +13,7 @@ import java.io.IOException;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.FileReader;
+import java.io.FileInputStream;
 import java.net.HttpURLConnection;
 import java.util.Map;
 import java.util.HashMap;
@@ -22,9 +23,10 @@ import java.util.HashMap;
 public final class NewsTest {
 
   public final String FULL_HTML_PATH = "src/test/java/com/google/sps/data/fullNewsHTML.txt";
+  public final String EMPTY_HTML_PATH = "src/test/java/com/google/sps/data/emptyHTML.txt";
   public List<Article> fullArticleList = new ArrayList<>(); 
 
-  private String textToString(String filePath) throws IOException {
+  public static String textToString(String filePath) throws IOException {
     StringBuilder stringBuilder = new StringBuilder();
     try (BufferedReader inputReader = new BufferedReader(new FileReader(filePath))) {
       String inputLine;
@@ -37,7 +39,24 @@ public final class NewsTest {
     }
     return stringBuilder.toString();
   }
-  
+
+  @Before
+  public void setUp() {
+    // Populate fullArticleList.
+    fullArticleList.add(new Article(
+      "The latest Instagram trend: putting your name on photos of frogs, cats and Harry Styles",
+      "news.google.com/articles/CBMib2h0dHBzOi8vd3d3LnRoZWd1YXJkaWFuLmNvbS91cy1uZXdzLzIwMjAvanVsLzIwL2luc3RhZ3JhbS13aGF0LWZyb2ctYXJlLXlvdS1pbnNpZGUtdGhlLWxhdGVzdC13YXktdG8td2FzdGUtdGltZdIBb2h0dHBzOi8vYW1wLnRoZWd1YXJkaWFuLmNvbS91cy1uZXdzLzIwMjAvanVsLzIwL2luc3RhZ3JhbS13aGF0LWZyb2ctYXJlLXlvdS1pbnNpZGUtdGhlLWxhdGVzdC13YXktdG8td2FzdGUtdGltZQ?hl=en-US&amp;gl=US&amp;ceid=US%3Aen"
+    ));
+    fullArticleList.add(new Article(
+      "How a blue protein turns tree frogs bright green",
+      "news.google.com/articles/CBMiVmh0dHBzOi8vd3d3LnNjaWVuY2VtYWcub3JnL25ld3MvMjAyMC8wNy9ob3ctYmx1ZS1wcm90ZWluLXR1cm5zLXRyZWUtZnJvZ3MtYnJpZ2h0LWdyZWVu0gEA?hl=en-US&amp;gl=US&amp;ceid=US%3Aen"
+    ));
+    fullArticleList.add(new Article(
+      "How frogs became green — again, and again, and again : Research Highlights", 
+      "news.google.com/articles/CBMiMmh0dHBzOi8vd3d3Lm5hdHVyZS5jb20vYXJ0aWNsZXMvZDQxNTg2LTAyMC0wMjEwMy160gEA?hl=en-US&amp;gl=US&amp;ceid=US%3Aen"
+    ));
+  }
+
   @Test
   public void articlesFromEmptyHTML() throws IOException {
     NewsServlet ns = new NewsServlet();
@@ -48,48 +67,43 @@ public final class NewsTest {
 
   @Test
   public void articlesFromFullHTML() throws IOException {
-    String link1 = "news.google.com/articles/CBMib2h0dHBzOi8vd3d3LnRoZWd1YXJkaWFuLmNvbS91cy1uZXdzLzIwMjAvanVsLzIwL2luc3RhZ3JhbS13aGF0LWZyb2ctYXJlLXlvdS1pbnNpZGUtdGhlLWxhdGVzdC13YXktdG8td2FzdGUtdGltZdIBb2h0dHBzOi8vYW1wLnRoZWd1YXJkaWFuLmNvbS91cy1uZXdzLzIwMjAvanVsLzIwL2luc3RhZ3JhbS13aGF0LWZyb2ctYXJlLXlvdS1pbnNpZGUtdGhlLWxhdGVzdC13YXktdG8td2FzdGUtdGltZQ?hl=en-US&amp;gl=US&amp;ceid=US%3Aen";
-    String link2 = "news.google.com/articles/CBMiVmh0dHBzOi8vd3d3LnNjaWVuY2VtYWcub3JnL25ld3MvMjAyMC8wNy9ob3ctYmx1ZS1wcm90ZWluLXR1cm5zLXRyZWUtZnJvZ3MtYnJpZ2h0LWdyZWVu0gEA?hl=en-US&amp;gl=US&amp;ceid=US%3Aen";
-    String link3 = "news.google.com/articles/CBMiMmh0dHBzOi8vd3d3Lm5hdHVyZS5jb20vYXJ0aWNsZXMvZDQxNTg2LTAyMC0wMjEwMy160gEA?hl=en-US&amp;gl=US&amp;ceid=US%3Aen";
-    fullArticleList.add(new Article("The latest Instagram trend: putting your name on photos of frogs, cats and Harry Styles", link1));
-    fullArticleList.add(new Article("How a blue protein turns tree frogs bright green", link2));
-    fullArticleList.add(new Article("How frogs became green — again, and again, and again : Research Highlights", link3));
     NewsServlet ns = new NewsServlet();
     String htmlString = textToString(FULL_HTML_PATH);
     List<Article> articleList = ns.makeArticleList(htmlString);
-    /*
-    System.out.println("Expected output");
-    for (Article article : fullArticleList) {
-      System.out.println(article.getTitle());
-      System.out.println(article.getLink());
-    }
-    System.out.println("\nActual output");
-    for (Article article : articleList) {
-      System.out.println(article.getTitle());
-      System.out.println(article.getLink());
-    }
-    */
-    for(int i = 0; i < 3; i++) {
+    // Check each class instance in each article object is equal.
+    Assert.assertEquals(articleList.size(), fullArticleList.size());
+    for(int i = 0; i < articleList.size(); i++) {
       Article realArticle = articleList.get(i);
       Article expectedArticle = fullArticleList.get(i);
-      if (!realArticle.getTitle().equals(expectedArticle.getTitle())) {
-        System.out.println(realArticle.getTitle() + " != " + expectedArticle.getTitle());
-      }
-      if (!realArticle.getLink().equals(expectedArticle.getLink())) {
-        System.out.println(realArticle.getLink() + " != " + expectedArticle.getLink());
-      }
-    }
-    // TODO: figure out why not equal
-    //Assert.assertEquals(articleList, fullArticleList);
+      Assert.assertEquals(expectedArticle.getTitle(), realArticle.getTitle());
+      Assert.assertEquals(expectedArticle.getLink(), realArticle.getLink());
+    }   
   }
-  /*
+
   @Test
-  public void badParamMap() {
-    Map<String, String> badParamMap = new HashMap<>();
-    badParamMap.put("lkdf", "kdn");
-    // TODO: Mock connection
-    HttpURLConnection connection = Mockito.mock(HttpURLConnection.class);
-    Mockito.when(connection.getInputStream()).thenReturn(new FileReader(FULL_HTML_PATH));
+  public void getEmptyHTML() {
+    try {
+      HttpURLConnection connection = Mockito.mock(HttpURLConnection.class);
+      Mockito.when(connection.getInputStream()).thenReturn(new FileInputStream(EMPTY_HTML_PATH));
+      NewsServlet ns = new NewsServlet();
+      Assert.assertEquals(ns.getHTML(connection), textToString(EMPTY_HTML_PATH));  
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
   }
-  */
+
+  @Test
+  public void getFullHTML() {
+    try {
+      HttpURLConnection connection = Mockito.mock(HttpURLConnection.class);
+      Mockito.when(connection.getInputStream()).thenReturn(new FileInputStream(FULL_HTML_PATH));
+      NewsServlet ns = new NewsServlet();
+      Assert.assertEquals(ns.getHTML(connection), textToString(FULL_HTML_PATH));  
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+
+  
+
 }
