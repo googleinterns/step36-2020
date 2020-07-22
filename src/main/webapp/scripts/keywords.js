f014d64c8bc54d3072cad9401fb8cb40151f950c
Juan Uribe <juanjouribev1@gmail.com>
2020-07-22 01:09:49 +0000

    Preload blobUrl

--- src/main/webapp/scripts/keywords.js
+++ src/main/webapp/scripts/keywords.js
@@ -1,17 +1,18 @@
+const BLOB_URL_PROMISE = loadTemplate('/user-image');
+
 $().ready(function() {
-  $('#select-blob').on('click', function() {
-    fetch('/user-image').then(response => response.text()).then((blobURL) => {
-      $('#keyword-form, #blob-input').removeClass('hide');
-      $('#keyword-form').attr('action', blobURL).attr('enctype', 'multipart/form-data');
-      $('#text-input').addClass('hide').val(null);
-      $('#form-title *').text('Upload an Image Here');
-    });
+  $('#select-blob').on('click', async function() {
+    const blobURL = await BLOB_URL_PROMISE;
+    $('#keyword-form, #blob-input, #submit-form').removeClass('hide');
+    $('#keyword-form').attr('action', blobURL).attr('enctype', 'multipart/form-data');
+    $('#text-input').addClass('hide').val(null);
+    $('#form-title > p').text('Upload an Image Here');
   });
 
   $('#select-text').on('click', function() {
-    $('#keyword-form, #text-input').removeClass('hide');
+    $('#keyword-form, #text-input, #submit-form').removeClass('hide');
     $('#keyword-form').attr('action', '/keyword').attr('enctype', 'application/x-www-form-urlencoded');
     $('#blob-input').addClass('hide').val(null);
-    $('#form-title *').text('Copy Your Tweet Here');
+    $('#form-title > p').text('Copy Your Tweet Here');
   });
 });
