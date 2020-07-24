a481e12bba2bae1d765410c1820c2933ad1d5e56
Nathan Safir <nssafir@gmail.com>
2020-07-24 19:00:08 +0000

    Make frontend for manual address input.

--- src/main/webapp/scripts/index.js
+++ src/main/webapp/scripts/index.js
@@ -6,4 +6,13 @@ $().ready(function() {
       eraseCookie('location');
     }
   });
+  $('#address').val(getCookie("address"));
+});
+
+$('#address').keypress(function(event){
+  var keycode = (event.keyCode ? event.keyCode : event.which);
+  if(keycode == '13'){
+    var address = $('#address').val();
+    setCookie('address', address, 1);  
+  }
 });
