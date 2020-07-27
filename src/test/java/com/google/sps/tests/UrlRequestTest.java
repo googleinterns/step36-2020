a76ad439d8f9bed383721d35321192c624900080
Juan Uribe <juanjouribev1@gmail.com>
2020-07-27 19:58:53 +0000

    Remove unnecessary import

--- src/test/java/com/google/sps/tests/UrlRequestTest.java
+++ src/test/java/com/google/sps/tests/UrlRequestTest.java
@@ -16,7 +16,6 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.junit.runners.JUnit4;
 import org.mockito.Mockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
