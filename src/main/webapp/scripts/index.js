8665f57541c8c2c94ec6811eba526d251a16028c
Nathan Safir <nssafir@gmail.com>
2020-07-27 19:15:23 +0000

    Make Juans requested PR changes.

--- src/main/webapp/scripts/index.js
+++ src/main/webapp/scripts/index.js
@@ -9,11 +9,7 @@ $().ready(function() {
   $('#address').val(getCookie("address"));
 });
 
-$('#address').keypress(function(event){
-  let keycode = (event.keyCode ? event.keyCode : event.which);
-  // If enter key is pressed for address input, store the address as a cookie.
-  if(keycode == '13'){
-    let address = $('#address').val();
-    setCookie('address', address, 1);  
-  }
+$('submit-form').click(function () {
+  let address = $('#address').val();
+  setCookie('address', address, 1);  
 });
