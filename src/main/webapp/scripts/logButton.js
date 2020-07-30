1a9af2325933a56ad57b4c15def81f8bdf267acb
Andrew Li <andrewhanzhuoli@gmail.com>
2020-07-30 18:33:37 +0000

    Fix comment typo

--- src/main/webapp/scripts/logButton.js
+++ src/main/webapp/scripts/logButton.js
@@ -1,9 +1,9 @@
 const LOG_URL_PROMISE = loadObject('/login');
 
 $().ready(async function() {
-  const logURL = await LOG_URL_PROMISE;
+  let logURL = await LOG_URL_PROMISE;
   $('#log-btn').attr('href', logURL[0]);
-  if (logURL.length == 2) {  // User is not loggeed in.
+  if (logURL.length == 2) {  // User is not logged in.
     $('#form-title > p').text('You are not logged in.');
     $('#log-btn').text("Login");
     $('#keyword-form, #input-select, #location-input, #previous-keywords, #address-input').addClass('hide');
