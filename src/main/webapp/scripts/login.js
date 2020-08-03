7eb24caac50c170e6f1a34c1af3a4625159178c4
Juan Uribe <juanjouribev1@gmail.com>
2020-08-03 18:33:24 +0000

    Refactor login load to fix race condition

--- src/main/webapp/scripts/login.js
+++ src/main/webapp/scripts/login.js
@@ -1,16 +1,19 @@
+const FORM_HTML_PROMISE = loadTemplate('/templates/inputForm.html');
 const LOG_URL_PROMISE = loadObject('/login');
 
 $().ready(async function() {
   let logURL = await LOG_URL_PROMISE;
   $('#log-btn').attr('href', logURL[0]);
   if (logURL.length == 2) {  // User is not logged in.
-    $('#form-title > p').text('You are not logged in.');
+    $('#form-title > p').text('You are not logged in');
     $('#log-btn').text("Log In");
-    $('#keyword-form, #input-select, #location').addClass('hide');
   } else {
     $('#form-title > p').text('Choose Type of Input');
     $('#log-btn').text('Log Out');
-    $('#input-select, #location').removeClass('hide');
+    $('#content').append(await FORM_HTML_PROMISE);
+    loadIndexListeners();
+    loadKeywordsFormListeners();
   }
   $('#log-btn').removeClass('hide');
+  $('body').removeClass('loading');
 });
