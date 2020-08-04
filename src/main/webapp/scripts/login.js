390c2be6d2d4fd2cdc1c192b57f0b3d2e4dc476e
Andrew Li <andrewhanzhuoli@gmail.com>
2020-08-04 20:24:05 +0000

    FIx login.js, login/logout button on results page

--- src/main/webapp/scripts/login.js
+++ src/main/webapp/scripts/login.js
@@ -3,6 +3,7 @@ const LOG_URL_PROMISE = loadObject('/login');
 
 $().ready(async function() {
   let logURL = await LOG_URL_PROMISE;
+  console.log("Log URL: " + logURL);
   $('#log-btn').attr('href', logURL[0]);
   if (logURL.length == 2) {  // User is not logged in.
     $('#form-title > p').text('You are not logged in');
@@ -11,8 +12,10 @@ $().ready(async function() {
     $('#form-title > p').text('Choose Type of Input');
     $('#log-btn').text('Log Out');
     $('#content').append(await FORM_HTML_PROMISE);
-    loadIndexListeners();
-    loadKeywordsFormListeners();
+    if ($('index').length) {  // Index page.
+      loadIndexListeners();
+      loadKeywordsFormListeners();
+    }
   }
   $('#log-btn').removeClass('hide');
   $('body').removeClass('loading');
